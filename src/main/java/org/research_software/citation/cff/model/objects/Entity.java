563174431a0b165692e20b1dc22990219d2d4482
Stephan Druskat <mail@sdruskat.net>
2018-02-02 13:56:59 +0100

    Add Javadoc

--- src/main/java/org/research_software/citation/cff/model/objects/Entity.java
+++ src/main/java/org/research_software/citation/cff/model/objects/Entity.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2018ff. Stephan Druskat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
 package org.research_software.citation.cff.model.objects;
 
 import java.net.MalformedURLException;
@@ -10,7 +27,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * // TODO Add description
+ * Model element representing an entity, e.g., a project group,
+ * organization, publishing house, conference, or similar.
+ * 
+ * @see <https://citation-file-format.github.io/1.0.3/specifications/#/entity-objects>
  * 
  * @version Citation File Format Version 1.0.3
  *
@@ -26,23 +46,25 @@ public final class Entity extends Subject {
 	private String location;
 
 	/**
-	 *
-	 * @param location
-	 * @param name
-	 * @param dateEnd
-	 * @param dateStart
-	 * @param address
-	 * @param city
-	 * @param region
-	 * @param postCode
-	 * @param country
-	 * @param orcid
-	 * @param email
-	 * @param tel
-	 * @param fax
-	 * @param website
-	 * @throws CFFModelException
-	 * @throws MalformedURLException
+	 * Constructor for databinding.
+	 * 
+	 * @param location The location of the entity
+	 * @param name The name of the entity (**required**)
+	 * @param dateEnd The end date, if the entity is an event, e.g., a conference
+	 * @param dateStart The start date, if the entity is an event, e.g., a conference
+	 * @param address The address of the entity
+	 * @param city The city where the entity is located
+	 * @param region The region where the entity is located
+	 * @param postCode The post code ("zip code") of the city where the entity is located
+	 * @param country The country where the entity is located
+	 * @param orcid The ORCID iD of the entity
+	 * @param email The email address of the entity
+	 * @param tel The telephone number of the entity
+	 * @param fax The telefax number of the entity 
+	 * @param website The website of the entity
+	 * 
+	 * @throws CFFModelException on invalid model values
+	 * @throws MalformedURLException on malformed URLs according to Java specs
 	 */
 	public Entity(@JsonProperty("name") String name, @JsonProperty("date-start") String dateStart,
 			@JsonProperty("date-end") String dateEnd, @JsonProperty("location") String location,
@@ -61,6 +83,9 @@ public final class Entity extends Subject {
 		this.location = location;
 	}
 
+	/**
+	 * @return The name of the entity
+	 */
 	@JsonProperty("name")
 	public String getName() {
 		return name;
@@ -71,6 +96,9 @@ public final class Entity extends Subject {
 		this.name = name;
 	}
 
+	/**
+	 * @return The start date of the entity
+	 */
 	@JsonProperty("date-start")
 	public LocalDate getDateStart() {
 		return dateStart;
@@ -88,6 +116,9 @@ public final class Entity extends Subject {
 		}
 	}
 
+	/**
+	 * @return The end date of the entity
+	 */
 	@JsonProperty("date-end")
 	public LocalDate getDateEnd() {
 		return dateEnd;
@@ -105,6 +136,9 @@ public final class Entity extends Subject {
 		}
 	}
 
+	/**
+	 * @return The location of the entity
+	 */
 	@JsonProperty("location")
 	public String getLocation() {
 		return location;
