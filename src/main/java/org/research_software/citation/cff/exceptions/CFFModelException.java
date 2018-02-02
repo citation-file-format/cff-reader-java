2cbdeb1f9a17cf95f9d82622874cb9ff5efc2b4a
Stephan Druskat <mail@sdruskat.net>
2018-02-02 13:39:32 +0100

    Add Javadoc

--- src/main/java/org/research_software/citation/cff/exceptions/CFFModelException.java
+++ src/main/java/org/research_software/citation/cff/exceptions/CFFModelException.java
@@ -1,10 +1,24 @@
 /**
- * 
+ * Copyright (c) 2018ff. Stephan Druskat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
  */
 package org.research_software.citation.cff.exceptions;
 
 /**
- * // TODO Add description
+ * Exception thrown on model issues.
  *
  * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
  * 
@@ -17,19 +31,19 @@ public class CFFModelException extends IllegalArgumentException {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * // TODO Add description
+	 * Constructor accepting a message.
 	 * 
-	 * @param message
+	 * @param message The message accompanying a throw of the exception.
 	 */
 	public CFFModelException(String message) {
 		this(message, null);
 	}
 
 	/**
-	 * // TODO Add description
+	 * Constructor accepting a message and a cause for the exception.
 	 * 
-	 * @param message
-	 * @param cause
+	 * @param message The message accompanying a throw of the exception.
+	 * @param cause The cause of the exception (a {@link Throwable}).
 	 */
 	public CFFModelException(String message, Throwable cause) {
 		super(message, cause);
--- src/main/java/org/research_software/citation/cff/exceptions/CFFReaderException.java
+++ src/main/java/org/research_software/citation/cff/exceptions/CFFReaderException.java
@@ -1,10 +1,24 @@
 /**
- * 
+ * Copyright (c) 2018ff. Stephan Druskat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
  */
 package org.research_software.citation.cff.exceptions;
 
 /**
- * // TODO Add description
+ * Exception thrown on issues during read.
  *
  * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
  * 
@@ -12,10 +26,15 @@ package org.research_software.citation.cff.exceptions;
 public class CFFReaderException extends Exception {
 
 	/**
-	 * 
+	 * The default serial version UID.
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor accepting a message.
+	 * 
+	 * @param message The message accompanying a throw of the exception.
+	 */
 	public CFFReaderException(String message) {
 		super(message);
 	}
